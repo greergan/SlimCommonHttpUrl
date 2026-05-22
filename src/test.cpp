1d0c418ae3b9a21ad53ebb6c9f8e4a2a6e56da34
Jeff Greer <greergan@gmail.com>
2026-05-21 18:40:50 -0700

    URL().can_parse passes scheme only tests
     Changes to be committed:
    	modified:   include/slim/common/http/url.h.in
    	modified:   required_packages
    	modified:   src/main.cpp
    	modified:   src/test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -1,5 +1,38 @@
-#include <slim/common/log.h>
+#include <format>
+#include <set>
 #include <slim/common/http/url.h>
+#include <slim/common/log.h>
+#include <slim/SlimValue.hpp>
 
 int main() {
+	using namespace slim::common;
+	std::set all_urls = {
+		"",
+		"a",
+		"a:",
+		"a:/",
+		"a://",
+		"file://"
+	};
+    log::trace(log::Message{__func__, "begins", __FILE__, __LINE__});
+	
+	for(auto& url : all_urls) {
+		auto parsable = http::URL::can_parse(url);
+		log::debug(log::Message{__func__, std::format("can_parse({}) => {}", url, parsable.to_string()), __FILE__, __LINE__});
+		if(parsable.has_error()) {
+			log::error(log::Message{__func__, std::format("can_parse({}) => {}", url, parsable.get_error().message()), __FILE__, __LINE__});
+		}
+
+		if(parsable.has_map("errors")) {
+ 			auto error_map = parsable.get_map("errors").get();
+			if(error_map.size() > 0) {
+				for(auto& [key, value] : error_map) {
+					log::error(log::Message{__func__, std::format("can_parse({}) => {} : {}", url, key, value.to_string()), __FILE__, __LINE__});
+				}
+			}
+		}
+	}
+
+	log::trace(log::Message{__func__, "ends", __FILE__, __LINE__});
+	return 0;
 }
